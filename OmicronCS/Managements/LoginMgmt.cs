bfae4ead7b3afc1eafd735071af736018b395ba0
Melwin <melwinoro@yahoo.com>
2015-04-01 20:52:59 +0800

    login

---
+++ OmicronCS/Managements/LoginMgmt.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+
+using OmicronCS_Constants;
+using MetroFramework;
+
+namespace OmicronCS_LoginMgmt
+{
+    public class LoginMgmt
+    {
+        public bool isLoginCredentialsValid(string username, string password)
+        {
+            return username.Equals(Constants.Username) && password.Equals(Constants.Password);
+        }
+
+        public void showLoginError(IWin32Window iWin32Window)
+        {
+            MetroMessageBox.Show(iWin32Window, "Incorrect username or password.",
+                                               "Login Error",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error,
+                                               Constants.LoginMessageBoxHeight);
+        }
+
+        public void clearLoginTextBox(TextBox textBoxUsername, TextBox textBoxPassword)
+        {
+            textBoxUsername.Clear();
+            textBoxPassword.Clear();
+            textBoxUsername.Focus();
+        }
+    }
+}
